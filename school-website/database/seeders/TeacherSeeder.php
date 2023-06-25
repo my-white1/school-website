52c52d0214bf49a001727bee8dce7bc5f89d47e6
Ahmadullo26062007 <113325761+Ahmadullo26062007@users.noreply.github.com>
2023-06-25 19:33:54 +0500

    seeder boldi

--- school-website/database/seeders/TeacherSeeder.php
+++ school-website/database/seeders/TeacherSeeder.php
@@ -14,11 +14,11 @@ class TeacherSeeder extends Seeder
     public function run(): void
     {
         Teacher::insert([
-            ['firstname'=>"jeny",'lastname'=>"jenies",'category'=>'matematika','image'=>''],
-            ['firstname'=>"jeny1",'lastname'=>"jenies1",'category'=>'inglis tili','image'=>''],
-            ['firstname'=>"jeny2",'lastname'=>"jenies2",'category'=>'rus tili','image'=>''],
-            ['firstname'=>"jeny3",'lastname'=>"jenies3",'category'=>'ona tili','image'=>''],
-            ['firstname'=>"jeny4",'lastname'=>"jenies4",'category'=>'fizika','image'=>''],
+            ['firstname'=>"jeny",'lastname'=>"jenies",'category'=>'matematika','image'=>'20492370.jpg'],
+            ['firstname'=>"jeny1",'lastname'=>"jenies1",'category'=>'inglis tili','image'=>'20492370.jpg'],
+            ['firstname'=>"jeny2",'lastname'=>"jenies2",'category'=>'rus tili','image'=>'20492370.jpg'],
+            ['firstname'=>"jeny3",'lastname'=>"jenies3",'category'=>'ona tili','image'=>'20492370.jpg'],
+            ['firstname'=>"jeny4",'lastname'=>"jenies4",'category'=>'fizika','image'=>'20492370.jpg'],
         ]);
     }
 }
